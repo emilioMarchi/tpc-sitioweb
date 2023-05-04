bf529414390cd3900d61818bb445f058b7b2b57a
emilioMarchi <em.98.nmynm@gmail.com>
2023-05-04 18:15:52 -0300

    commit

--- src/components/home/homeCarousel/homeCarouselItem/HomeCarouselItem.js
+++ src/components/home/homeCarousel/homeCarouselItem/HomeCarouselItem.js
@@ -2,16 +2,14 @@ import './homeCarouselItem.css'
 
 export const HomeCarouseItem = ({data}) => {
     return(
-        <div className='home-carousel-item'>
+        <div className='home-carousel-item' style={{backgroundImage: 'url(/img/img.png)'}}>
             <div className='col carousel-item-row title'>
                 <h2>{data.title}</h2>
                 <h3>{data.description}</h3>
                 <button className='carousel-item-button btn'>Conocer más</button>
                 
             </div>
-            <div className='col carousel-item-row'>
-                <img src='/img/img.png' />
-            </div>
+            
         </div>
     )
 }
